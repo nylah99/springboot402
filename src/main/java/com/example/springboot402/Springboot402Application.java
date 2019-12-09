738543d8634fd34686ed57fbfe4c3d00398af2db
Nylah Safforld <nylahsafforld@Nylahs-MacBook-Air.local>
2019-12-09 14:18:51 -0500

    Initial commit

---
+++ src/main/java/com/example/springboot402/Springboot402Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot402Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot402Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot402/Springboot402ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot402;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot402ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
